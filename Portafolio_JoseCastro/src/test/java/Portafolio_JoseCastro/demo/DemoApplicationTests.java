dd9cce938f567d28953e101e5a16eed55938bf69
MichiComputadoraPCGato <107232365+Pochilito@users.noreply.github.com>
2026-09-16 20:38:51 -0600

    semana2

---
+++ Portafolio_JoseCastro/src/test/java/Portafolio_JoseCastro/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_JoseCastro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
